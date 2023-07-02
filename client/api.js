31a9aaff3efdf3b2c8fcb6aab726f3885b518f7f
dawit <dawitbogalem@gmail.com>
2023-07-02 17:37:48 -0400

    Co-authored-by: xjqiu28 <xjqiu28@users.noreply.github.com> Co-authored-by: Richard <daorichard@users.noreply.github.com> Co-authored-by: sebastiansarm <sebastiansarm@users.noreply.github.com>

---
+++ client/api.js
@@ -0,0 +1,11 @@
+const fetchFromYelpURL =
+  'api.yelp.com/v3/businesses/search?location=${locationSearched}`';
+
+export const BEARER_TOKEN = `
+3fL-Z1zOIZnAWPYAVo462Sbf4R9ODxQ0CQXTW2KXAFRQeLuHyX38jrBGDIeplBuy04evgJvqVMj3zUhR1mFN2WzaxnyVRwvwboqceelX108pa3gL2jFOoyTXnj-fZHYx`;
+
+export const BEARER_TOKEN2 = `
+CuFmwamx8ZGyAJq4-jmFkFSEo-yld8-BzkNXX8qLVHn5NTv9LUrq2lB13q5LhS9hKBdDGrB2Xcogn6e1PPR1s51YazYpOE9re3-PrcKv6us-stSyvzNhW1tVLWufZHYx`;
+
+export const BEARER_TOKEN3 = `
+0RTPAt-uwknNr0K_DrBpHOsU51HqCQIKuyVFD96OklSfwtIAYdHGwR55QsLy0AGXaoZhS2ePAgWAgH96z7v0xFkWetVJUNKifHo2xpLOK2eN7WVcyW0DChWde2ufZHYx`;
--- client/components/main.jsx
+++ client/components/main.jsx
@@ -1,8 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Card from './Card';
 import './main.scss';
+import axios from 'axios';
+import { BEARER_TOKEN } from '../api.js';
 
 const Main = (props) => {
+  // const {zipCode} = props
+
+  const dummyZip = '20009';
+  const [fetchedData, setFetchedData] = useState([]);
+
+  const fetchYelp = () => {
+    axios
+      .get(
+        `${'https://cors-anywhere.herokuapp.com/'}https://api.yelp.com/v3/businesses/search?location=${dummyZip}`,
+        {
+          headers: {
+            Authorization: `Bearer ${BEARER_TOKEN}`,
+          },
+          params: {
+            categories: 'breakfast_brunch',
+          },
+        }
+      )
+      .then((res) => {
+        console.log(res);
+      })
+      .catch((err) => {
+        console.log('error');
+      });
+  };
+
   return (
     <article className='main'>
       <Card />
