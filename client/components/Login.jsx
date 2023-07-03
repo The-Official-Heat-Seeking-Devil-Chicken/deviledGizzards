479397fb1d5a40fdb133ffbc52b8f00cc412d1dd
xjqiu28 <76609392+xjqiu28@users.noreply.github.com>
2023-07-03 11:23:02 -0400

    login page logs in when username password correct

--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -1,13 +1,12 @@
-import React, { useState, useEffect, useRef } from 'react';
-import { useNavigate } from 'react-router-dom';
-import './login.scss';
+import React, { useState, useEffect, useRef } from "react";
+import { useNavigate } from "react-router-dom";
+import "./login.scss";
 
 function Login() {
   //direct you anywhere as long as you have specified that path before
   const navigate = useNavigate();
 
-
-  const URL = "http://localhost:3000/";
+  const URL = "http://localhost:3000/login";
 
   const usernameRef = useRef(null);
   const passwordRef = useRef(null);
@@ -18,10 +17,10 @@ function Login() {
     const password = passwordRef.current.value;
 
     fetch(URL, {
-      method: 'GET',
-      mode: 'cors',
+      method: "POST",
+      mode: "cors",
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
       body: JSON.stringify({
         username: username,
@@ -29,39 +28,40 @@ function Login() {
       }),
     })
       .then((res) => {
-        console.log("This is res:", res);
         return res.json();
       })
       .then((data) => {
-        console.log("This is inside the then data")
-        navigate('/home')
-      }).
-      catch(err =>{
-
-        console.log("Need to doublecheck username/password")
+        if (data.error) {
+          alert("Wrong Username/Password")
+        } else {
+          navigate("/home");
+        }
+      })
+      .catch((err) => {
+        console.log("Need to doublecheck username/password");
       });
   };
 
   return (
-    <div className='login-background'>
-      <form onSubmit={handleSubmit} className='login-form'>
-        <p className='title'>ME WANT FOOD</p>
+    <div className="login-background">
+      <form onSubmit={handleSubmit} className="login-form">
+        <p className="title">ME WANT FOOD</p>
         <input
           ref={usernameRef}
-          className='login-input'
-          name='username'
-          type='text'
-          placeholder='Username'
+          className="login-input"
+          name="username"
+          type="text"
+          placeholder="Username"
         />
         <input
           ref={passwordRef}
-          className='login-input'
-          name='password'
-          type='password'
-          placeholder='Password'
+          className="login-input"
+          name="password"
+          type="password"
+          placeholder="Password"
         />
         <button>Login</button>
-        <a href='/signup'>SIGN UP</a>
+        <a href="/signup">SIGN UP</a>
       </form>
     </div>
   );
